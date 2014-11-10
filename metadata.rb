77844ab419c946e06c7f294af8932f5ca1273cf1
Ken Joyner <Ken@KJoyner.com>
2014-11-09 22:45:21 -0800

    ...

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-name             "rails_secrets_from_s3"
-maintainer       "kjoyner"
-maintainer_email "kjoyner"
-license          "Apache 2.0"
-description      "Installs/Configures rails_secrets_from_s3"
-long_description "Installs/Configures rails_secrets_from_s3"
-version          "0.1.0"
-depends          "citadel"
-
+name             'rails_secrets_from_s3'
+maintainer       'kjoyner'
+maintainer_email 'kjoyner'
+license          'Apache 2.0'
+description      'Sets up the Rails secrets.yml from S3'
+long_description 'Sets up the Rails secrets.yml from S3'
+version          '0.1.0'
+# depends          'citadel'
